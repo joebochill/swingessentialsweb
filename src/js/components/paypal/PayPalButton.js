d984fe370b1a20e826ce19d5ebf298e9d9582cdd
joebochill <boyle.p.joseph@gmail.com>
2018-01-18 23:23:33 -0500

    first attempt

--- src/js/components/paypal/PayPalButton.js
+++ src/js/components/paypal/PayPalButton.js
@@ -27,6 +27,9 @@ class PaypalButton extends Component {
     }
     document.getElementById('ppbutton').innerHTML = "";
 
+    let CREATE_PAYMENT_URL = 'http://www.josephpboyle.com/api/myapi.php/createpayment';
+    let EXECUTE_PAYMENT_URL = 'http://www.josephpboyle.com/api/myapi.php/executepayment';
+    
     paypal.Button.render({
         env: 'sandbox',
         style: {
@@ -37,49 +40,64 @@ class PaypalButton extends Component {
             fundingicons: true
         },
         client: {
-        // from https://developer.paypal.com/developer/applications/
-            sandbox:    'AUdpw5-vad4rjjbmn52xP5Gw_8NfZO14Ff0iB0GvjN2cvjFNvq-9yzgVuAUxckNVIf9VDRQCnd8OF2Vg',
-            production: 'xxxxxxxxx'  
-        },
+            // from https://developer.paypal.com/developer/applications/
+                sandbox:    'AUdpw5-vad4rjjbmn52xP5Gw_8NfZO14Ff0iB0GvjN2cvjFNvq-9yzgVuAUxckNVIf9VDRQCnd8OF2Vg',
+                production: 'xxxxxxxxx'  
+            },
         // See https://developer.paypal.com/docs/api/payments/#payment_create 
         payment: (data, actions) =>
-            actions.payment.create({
-                intent: "sale",
-                payment: {
-                    transactions: [
-                    {
-                        amount: {
-                            total: this.props.total,
-                            currency: "USD",
-                            details: {
-                                subtotal: this.props.total,
-                                tax: "0.00"
+                actions.payment.create({
+                    intent: "sale",
+                    payment: {
+                        transactions: [
+                        {
+                            amount: {
+                                total: this.props.total,
+                                currency: "USD",
+                                details: {
+                                    subtotal: this.props.total,
+                                    tax: "0.00"
+                                }
+                            },
+                            description: "Golf Swing Analysis",
+                            item_list: {
+                                items: [
+                                {
+                                    name: this.props.deal.name,
+                                    description: this.props.deal.description,
+                                    quantity: 1,
+                                    price: this.props.total,
+                                    tax: "0.00",
+                                    currency: "USD"
+                                }
+                                ]
                             }
-                        },
-                        description: "Golf Swing Analysis",
-                        item_list: {
-                            items: [
-                            {
-                                name: this.props.deal.name,
-                                description: this.props.deal.description,
-                                quantity: 1,
-                                price: this.props.total,
-                                tax: "0.00",
-                                currency: "USD"
-                            }
-                            ]
+                        }]
+                    },
+                    experience: {
+                        input_fields: {
+                            no_shipping: 1
                         }
-                    }]
-                },
-                experience: {
-                    input_fields: {
-                        no_shipping: 1
                     }
-                }
-            }),
+                }),
+        // payment: function(){
+        //     return paypal.request.post(CREATE_PAYMENT_URL).then(function(data){
+        //         return data.id;
+        //     });
+        // },
         commit: true,
-        onAuthorize: this.props.authorized,//(data, actions)=>{}
-        onCancel: this.props.canceled //(data)=>{}
+        // onAuthorize: (data) =>{
+        //     return paypal.request.post(EXECUTE_PAYMENT_URL, {
+        //         paymentID: data.paymentID,
+        //         payerID: data.payerID
+        //     })
+        //     .then(()=>
+        //         alert('success: payment complete')
+        // )
+        // },
+        //onAuthorize: this.props.authorized,//(data, actions)=>{}
+        onAuthorize: (data, actions) => this.props.executePayment({paymentID: data.paymentID, payerID: data.payerID}),
+        onCancel:(data)=>console.log(data)// this.props.canceled //(data)=>{}
     }, '#ppbutton');
   }
 
