284086a089aa63af3ab80105131a8c4c86ba3a8f
Joseph Boyle <boyle.p.joseph@gmail.com>
2019-07-22 19:29:39 -0400

    Add version number to footer

--- src/js/components/footer/Footer.js
+++ src/js/components/footer/Footer.js
@@ -5,9 +5,18 @@ class Footer extends Component {
   render() {
     const year = (new Date()).getFullYear();
     return (
-      <section className="se_footer">
-        <p>{`Copyright © Swing Essentials, LLC. ${year - 1} - ${year}`}</p>
+      <section className="se_footer" style={{position: 'relative'}}>
+        <p>{`Copyright © Swing Essentials, LLC. ${year} - ${year + 1}`}</p>
         <p><NavLink to='/legal/privacy'>Privacy Policy</NavLink>&nbsp;&nbsp;&nbsp;&nbsp;<NavLink to='/legal/terms'>Terms of Use</NavLink></p>
+        <p style={{
+            position: 'absolute', 
+            width: 'auto',
+            right: '0.5rem', 
+            bottom: '0.5rem', 
+            fontSize: '0.65rem',
+            lineHeight: 1,
+            opacity: 0.5
+          }}>{`v 2.1.0`}</p>
       </section>
     );
   }
